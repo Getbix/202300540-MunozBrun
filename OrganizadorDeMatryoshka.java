95126f041857f8e131e38287d5004f4bfdc6c148
Gabriel Enrique Muñoz Brun <128784777+Getbix@users.noreply.github.com>
2024-06-18 00:55:22 -0400

    OrganizadorDeMatryoshka.java

--- OrganizadorDeMatryoshka.java
+++ OrganizadorDeMatryoshka.java
@@ -1,28 +1,41 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class OrganizadorDeMatryoshka{
     public static String[][] ordenar(String[] s){
-        Arrays.sort(s);
-        List<List<String>> resultadoListas = new ArrayList<>();
-        List<String> actualLista = new ArrayList<>();
-        
-        for(int i = 0; i < s.length; i++){
-            if(i == 0 || !s[i].equals(s[i - 1])){
-                actualLista.add(s[i]);
-            } else{
-                actualLista.add(s[i]);
-            }
-            if(i == s.length - 1 || !s[i].equals(s[i + 1])){
-                resultadoListas.add(new ArrayList<>(actualLista));
-                actualLista.clear();
+        List<String> listaMuñecasUnicas = new ArrayList<>();
+        for(String muñeca : s){
+            if(!listaMuñecasUnicas.contains(muñeca)){
+                listaMuñecasUnicas.add(muñeca);
             }
         }
-        String[][] resultado = new String[resultadoListas.size()][];
-        for (int i = 0; i < resultadoListas.size(); i++){
-            List<String> lista = resultadoListas.get(i);
-            resultado[i] = lista.toArray(new String[0]);
+        Collections.sort(listaMuñecasUnicas);
+        int[] conteos = new int[listaMuñecasUnicas.size()];
+        for(String muñeca : s){
+            int indice = listaMuñecasUnicas.indexOf(muñeca);
+            conteos[indice]++;
+        }
+        int numConjuntos = 0;
+        for(int conteo : conteos){
+            if(conteo > numConjuntos){
+                numConjuntos = conteo;
+            }
+        }
+        List<String>[] conjuntos = new List[numConjuntos];
+        for(int i = 0; i < numConjuntos; i++){
+            conjuntos[i] = new ArrayList<>();
+        }
+        int[] mapaIndices = new int[listaMuñecasUnicas.size()];
+        for(String muñeca : s){
+            int indiceUnico = listaMuñecasUnicas.indexOf(muñeca);
+            int indiceConjunto = mapaIndices[indiceUnico];
+            conjuntos[indiceConjunto].add(muñeca);
+            mapaIndices[indiceUnico]++;
+        }
+        String[][] resultado = new String[numConjuntos][];
+        for(int i = 0; i < numConjuntos; i++){
+            List<String> conjunto = conjuntos[i];
+            Collections.sort(conjunto);
+            resultado[i] = conjunto.toArray(new String[0]);
         }
         return resultado;
     }
